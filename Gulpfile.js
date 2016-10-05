1dc25bfa9cc0c63c96f646511f6c53b33b94530b
Erwan Osouf <erwan.osouf@gmail.com>
2016-10-05 23:09:03 +0200

    Update dir structure, add a gulp task for running a web server.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,14 +1,28 @@
 var gulp = require('gulp');
-var include = require('gulp-file-include');
-var changed = require('gulp-changed');
+var util = require('gulp-util');
+var static = require('node-static');
+var argv = require('yargs').argv;
+var http = require('http');
 
-var DEST = './build/';
+const DEST = './build/';
+const COURSES = './';
+const DEFAULT_PORT = 8080;
 
-gulp.task('includes', function() {
-    gulp.src('./pages/*.html')
-    .pipe(include({
-      prefix: '@@',
-      basepath: './fragments'
-    }))
-    .pipe(gulp.dest(DEST));
+// TODO Find if and how to stop server.
+gulp.task('serve', function() {
+  var file = new static.Server(COURSES), port = parseInt(argv.port) || DEFAULT_PORT, server;
+
+  gulp.on('end', function() {
+    util.log('Shutting down Server');
+    server.close()
+  });
+
+  server = http.createServer(function (request, response) {
+    request.addListener('end', function () {
+        file.serve(request, response);
+    }).resume();
+  });
+  server.listen(port)
+  util.log('Server running on port ' + port);
+
 });
