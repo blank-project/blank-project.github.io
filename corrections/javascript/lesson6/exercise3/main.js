9680b0563220a78427c37d153e427ff6497e2fab
Marin Procureur <marin.procureur@gmail.com>
2017-01-09 16:54:38 +0100

    exercise3 lesson6 js start correction

---
+++ corrections/javascript/lesson6/exercise3/main.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+  var benji = document.getElementById("benjamin"); //get button
+  benji.addEventListener("click", makeRequest); //associate request to button
+};
+
+//request function
+function makeRequest() {
+  var request = new XMLHttpRequest(); //create request object*
+  request.onreadystatechange = function() {
+    //if request ok
+    if (this.readyState == 4 && this.status == 200) {
+      handleRequest(request);
+    } else {
+      //if something went wrong
+      alert("something went wrong...");
+      console.log("request",request,"event",this);
+    }
+  };
+  request.open("GET","data.json",true); //opening request to retrieve data.json asynchronously
+  request.send(); //sending request
+}
+
+//handle request
+function handleRequest(request) {
+  console.log(request);
+}
