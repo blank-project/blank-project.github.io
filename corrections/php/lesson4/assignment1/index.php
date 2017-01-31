0eb51c1737baf63885d1d22474e2af242d21d62a
Marin Procureur <marin.procureur@gmail.com>
2017-01-31 17:04:49 +0100

    Add correction of global assignment

---
+++ corrections/php/lesson4/assignment1/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>connexion user</title>
+  </head>
+  <?php
+  require("_login.php");
+   ?>
+  <body>
+    <h1>WELCOME !</h1>
+  </body>
+</html>
