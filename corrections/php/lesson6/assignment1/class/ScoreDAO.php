3939354dc4e84a600af5d815060b64a61f9ab5ea
Marin Procureur <marin.procureur@gmail.com>
2017-02-24 18:16:05 +0100

    Update corrections assignment + sql lesson 3

--- corrections/php/lesson6/assignment1/class/ScoreDAO.php
+++ corrections/php/lesson6/assignment1/class/ScoreDAO.php
@@ -19,5 +19,25 @@
       return true;
     }
 
+
+    public function find($db, $game, $number) {
+      //prepare request
+      $request = $db->prepare("SELECT username, score FROM Score WHERE game = :game ORDER BY score DESC LIMIT :number");
+
+      //execute request
+      try {
+        $request->execute(array(
+          "game" => $game,
+          "number" => $number
+        ));
+        $result = $request->fetchAll();
+        return $result;
+      } catch (PDOException $e) {
+        print("error while writing in DB new user." . $e->getMessage());
+        return null;
+      }
+    }
+
+
   }
  ?>
--- corrections/php/lesson6/assignment1/scores.php
+++ corrections/php/lesson6/assignment1/scores.php
@@ -1,1 +1,43 @@
-<?php  echo "Score pour " . $_GET['game']; ?>
+<?php
+  require_once("_db.php");
+
+  spl_autoload_register(function($name) {
+    return require("class/" . $name . ".php");
+  });
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Scores</title>
+  </head>
+  <body>
+    <?php
+    if (isset($_REQUEST['game'])) {
+      $game = $_REQUEST['game'];
+      $dao = new ScoreDAO();
+      $scores = $dao->find($_DB, $game, 5);
+      echo "<h1>Scores pour $game</h1>";
+      echo "<table><tr><th>Utilisateur</th><th>Score</th></tr>";
+      foreach ($scores as $score) {
+        echo "<tr><td>" . $score["username"] ."</td><td>" . $score["score"] ."</td></tr>";
+      }
+      echo "</table>";
+    } else {
+      echo "<h1>Scores pour :</h1>";
+      ?>
+      <form action="scores.php">
+        <select name="game">
+          <option value="hangman">hangman</option>
+          <option value="musicbox">musicbox</option>
+        </select>
+        <button type="submit">Go !</button>
+      </form>
+      <?php
+    }
+    ?>
+    <a href="game.php">Game</a>
+    <a href="scores.php">Scores</a>
+  </body>
+</html>
