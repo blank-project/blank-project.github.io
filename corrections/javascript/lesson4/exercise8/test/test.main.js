e9b54d420ed63232607598eb5012abfdeda70d11
Marin Procureur <marin.procureur@gmail.com>
2018-01-23 12:00:30 +0100

    test for sumArray

--- corrections/javascript/lesson4/exercise8/test/test.main.js
+++ corrections/javascript/lesson4/exercise8/test/test.main.js
@@ -1,79 +1,26 @@
-describe('anneeBissextile', function() {
+describe('sumArray', function() {
 
   it('should be a function', function() {
-    anneeBissextile.should.be.a('function');
+    sumArray.should.be.a('function');
   });
 
-  it('should return true for 2000', function() {
-    anneeBissextile(2000).should.be.true;
+  it('should return 20 for the array [20]', function() {
+    sumArray([20]).should.be.equal(20);
   });
 
-  it('should return true for 2004', function() {
-    anneeBissextile(2004).should.be.true;
+  it('should return 25 for the array [20, 5]', function() {
+    sumArray([20, 5]).should.be.equal(25);
   });
 
-  it('should return true for 2001', function() {
-    anneeBissextile(2001).should.be.false;
+  it('should return 241 for the array [200, 40, 1]', function() {
+    sumArray([200, 40, 1).should.be.equal(241);
   });
 
 });
 
-describe('nombreDeJour', function() {
-
-  it('should be a function', function() {
-    nombreDeJour.should.be.a('function');
+describe('bonus', function() {
+  it('should not sum up elements of the table that are not numbers', funciton() {
+    sumArray(['olé']).should.be.equal(0);
+    sumArray(['true', 20, true, false]).should.be.equal(20);
   });
-
-  it('should return 31 for January', function() {
-    nombreDeJour(01, 2001).should.be.equal(31);
-  });
-
-  it('should return 28 for February 2001', function() {
-    nombreDeJour(02, 2001).should.be.equal(28);
-  });
-
-  it('should return 29 for February 2000', function() {
-    nombreDeJour(02, 2000).should.be.equal(29);
-  });
-
-  it('should return 31 for March', function() {
-    nombreDeJour(03, 2001).should.be.equal(31);
-  });
-
-  it('should return 30 for April', function() {
-    nombreDeJour(04, 2001).should.be.equal(30);
-  });
-
-  it('should return 31 for May', function() {
-    nombreDeJour(05, 2001).should.be.equal(31);
-  });
-
-  it('should return 30 for June', function() {
-    nombreDeJour(06, 2001).should.be.equal(30);
-  });
-
-  it('should return 31 for July', function() {
-    nombreDeJour(07, 2001).should.be.equal(31);
-  });
-
-  it('should return 31 for August', function() {
-    nombreDeJour(08, 2001).should.be.equal(31);
-  });
-
-  it('should return 30 for September', function() {
-    nombreDeJour(09, 2001).should.be.equal(30);
-  });
-
-  it('should return 31 for October', function() {
-    nombreDeJour(10, 2001).should.be.equal(31);
-  });
-
-  it('should return 30 for November', function() {
-    nombreDeJour(04, 2001).should.be.equal(30);
-  });
-
-  it('should return 31 for December', function() {
-    nombreDeJour(12, 2001).should.be.equal(31);
-  });
-
 });
