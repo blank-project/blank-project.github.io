8a2cd23b90108deca807963083910df38128538b
Marin Procureur <marin.procureur@gmail.com>
2018-02-05 10:59:49 +0100

    add untracked files (javascript correction lesson 6, javascript lesson8 network icon for main page)

--- corrections/javascript/lesson6/exercise2/script.js
+++ corrections/javascript/lesson6/exercise2/script.js
@@ -1,26 +1,49 @@
-
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    var data = JSON.parse(this.responseText), item;
-    var markup;
-    for (var i = 0; i < data.length; i++) {
-        item = data[i];
-        markup +=  render(item);
-    }
-    document.body.innerHTML = markup;
+var request = new XMLHttpRequest();
+request.onreadystatechange = function() {
+  if (this.readyState === 4 && this.status === 200) {
+    buildTondeuses(JSON.parse(this.responseText));
   }
 };
-xhttp.open("GET", "http://localhost:8000/data/data.json", true);
-xhttp.send();
 
-function render(item) {
-  return '<div class="card">'
-  + '<img class="card-img-top card-img" src="' + item.image.source + '" alt="' + item.image.alt + '" />'
-  +   '<div class="card-block">'
-  +     '<h4 class="card-title">' + item.name + '</h4>'
-  +     '<p class="card-text">' +  item.description + '</p>'
-  +     '<a href="#" class="btn btn-primary">' + item.price + " " + item.currency + '</a>'
-  +   '</div>'
-  + '</div>'
+request.open('GET', 'http://localhost:8000/data.json', true);
+request.send();
+
+function buildTondeuses(tondeuses) {
+  for (var i = 0; i < tondeuses.length; i++) {
+    build(tondeuses[i]);
+  }
+}
+
+function createContainer() {
+  var container = document.createElement('div');
+  container.classList.add('tondeuse');
+  return container;
+}
+
+function createTitle(title) {
+  var h1 = document.createElement('h1');
+  h1.innerText = title;
+  return h1;
+}
+
+function createImage(img) {
+  var image = document.createElement('img');
+  image.setAttribute('src', img.source);
+  image.setAttribute('alt', img.alt);
+  return image;
+}
+
+function build(t) {
+  var main = document.querySelector('main');
+  var container = createContainer();
+
+  // création du title
+  var title = createTitle(t.name);
+  container.appendChild(title);
+
+  // création de l'image
+  var image = createImage(t.image);
+  container.appendChild(image);
+
+  main.appendChild(container);
 }
