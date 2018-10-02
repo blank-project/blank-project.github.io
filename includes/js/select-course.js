2f417e101cb1debbf3d8f5cd026e63ca56a644e9
Marin Procureur <marin.procureur@gmail.com>
2018-10-02 17:35:10 +0200

    revamp front page of courses

---
+++ includes/js/select-course.js
@@ -0,0 +1,11 @@
+var cards = document.getElementsByClassName("card");
+
+for (var i = 0; i < cards.length; i++) {
+    if (cards[i] !== undefined) {
+        cards[i].addEventListener("click", handleCardClick.bind(cards[i]));
+    }
+}
+
+function handleCardClick() {
+    location.href = this.getAttribute("data-link");
+}
