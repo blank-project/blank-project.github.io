ed3b8586f206ee59d6e5f48ad4b9f6965dbfe841
Erwan Osouf <erwan.osouf@gmail.com>
2018-10-19 16:08:03 +0200

    Wrap in an IIFE

--- includes/js/select-course.js
+++ includes/js/select-course.js
@@ -1,11 +1,15 @@
-var cards = document.getElementsByClassName("card");
+(function() {
 
-for (var i = 0; i < cards.length; i++) {
-    if (cards[i] !== undefined) {
-        cards[i].addEventListener("click", handleCardClick.bind(cards[i]));
+    var cards = document.getElementsByClassName("card");
+
+    for (var i = 0; i < cards.length; i++) {
+        if (cards[i] !== undefined) {
+            cards[i].addEventListener("click", handleCardClick.bind(cards[i]));
+        }
     }
-}
 
-function handleCardClick() {
-    location.href = this.getAttribute("data-link");
-}
+    function handleCardClick() {
+        location.href = this.getAttribute("data-link");
+    }
+
+})();
